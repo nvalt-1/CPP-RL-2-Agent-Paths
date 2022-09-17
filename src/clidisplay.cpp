170093fe43aefea03cc7a9501ab4509facc11e2c
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2022-09-17 13:49:55 -0500

    removed redundant code

--- src/clidisplay.cpp
+++ src/clidisplay.cpp
@@ -12,49 +12,10 @@ draw(bool slow) {
     // std::cout << "CLIDisplay::" << __func__ << " ENTER\n";
     #ifndef _WIN32
     system("clear");
-    for(int y = 0; y < WorldConfig::instance()->worldSize; ++y) {
-        std::cout << "[";
-        for(int x = 0; x < WorldConfig::instance()->worldSize; ++x) {
-            Position pos(x, y);
-            bool isPickup = false;
-            for(int i = 0; i < WorldConfig::instance()->pickupPositions.size(); ++i) {
-                if(pos == WorldConfig::instance()->pickupPositions[i]) {
-                    isPickup = true;
-                    break;
-                }
-            }
-
-            bool isDropoff = false;
-            for(int i = 0; i < WorldConfig::instance()->dropoffPositions.size(); ++i) {
-                if(pos == WorldConfig::instance()->dropoffPositions[i]) {
-                    isDropoff = true;
-                    break;
-                }
-            }
-
-            bool hasAgent = false;
-            bool carryingBlock = false;
-            int agentID = -1;
-            for(int i = 0; i < World::instance()->agents.size(); ++i) {
-                if(pos == World::instance()->agents[i].pos) {
-                    hasAgent = true;
-                    carryingBlock = World::instance()->agents[i].carryingBlock;
-                    agentID = i;
-                    break;
-                }
-            }
-
-            std::cout << " " << (isPickup ? "P" : isDropoff ? "D" : " ") << 
-                (hasAgent ? carryingBlock ? altAgentChars[agentID] : agentChars[agentID] : sep[0]) << 
-                (x == WorldConfig::instance()->worldSize - 1 ? "" : "|");
-        }
-        std::cout << "]\n";
-    }
-    World::instance()->print();
     #endif
-
     #ifdef _WIN32
     setCursorPosition(0, 0);
+    #endif
     for(int y = 0; y < WorldConfig::instance()->worldSize; ++y) {
         std::cout << "[";
         for(int x = 0; x < WorldConfig::instance()->worldSize; ++x) {
@@ -94,8 +55,8 @@ draw(bool slow) {
         std::cout << "]\n";
     }
     World::instance()->print();
-    std::cout.flush();
-    #endif
+    std::cout.flush(); 
+
     if(slow) {
         #ifdef _WIN32
         Sleep(75);
